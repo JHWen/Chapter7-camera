188aaaa07e2679e2391b17a9d156c12bcd31d588
codesky <super_codesky@163.com>
2019-01-25 21:11:42 +0800

    change:第二个练习，实现视频拍摄功能

--- app/src/main/java/com/bytedance/camera/demo/RecordVideoActivity.java
+++ app/src/main/java/com/bytedance/camera/demo/RecordVideoActivity.java
@@ -3,10 +3,14 @@ package com.bytedance.camera.demo;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class RecordVideoActivity extends AppCompatActivity {
@@ -14,7 +18,8 @@ public class RecordVideoActivity extends AppCompatActivity {
     private VideoView videoView;
     private static final int REQUEST_VIDEO_CAPTURE = 1;
 
-    private static final int REQUEST_EXTERNAL_CAMERA = 101;
+
+    private static final int REQUEST_CAMERA_RECORD_AUDIO = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +27,70 @@ public class RecordVideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_record_video);
 
         videoView = findViewById(R.id.img);
-        findViewById(R.id.btn_picture).setOnClickListener(v -> {
-            if (ContextCompat.checkSelfPermission(RecordVideoActivity.this,
-                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                //todo 在这里申请相机、存储的权限
+
+        videoView.setOnClickListener(v -> {
+            if (videoView.isPlaying()) {
+                videoView.pause();
             } else {
-                //todo 打开相机拍摄
+                videoView.start();
             }
         });
 
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(RecordVideoActivity.this,
+                    Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(RecordVideoActivity.this,
+                            Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                //todo 在这里申请相机、存储的权限
+                ActivityCompat.requestPermissions(RecordVideoActivity.this,
+                        new String[]{Manifest.permission.CAMERA,
+                                Manifest.permission.RECORD_AUDIO},
+                        REQUEST_CAMERA_RECORD_AUDIO);
+
+            } else {
+                //todo 打开相机拍摄
+                takeVideo();
+            }
+        });
+
+    }
+
+    private void takeVideo() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
             //todo 播放刚才录制的视频
+            Uri videoUri = intent.getData();
+            videoView.setVideoURI(videoUri);
+            videoView.start();
         }
     }
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
-            case REQUEST_EXTERNAL_CAMERA: {
+            case REQUEST_CAMERA_RECORD_AUDIO: {
                 //todo 判断权限是否已经授予
+                for (int i = 0; i < grantResults.length; i++) {
+                    int state = grantResults[i];
+                    if (state == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(RecordVideoActivity.this, permissions[i] + " permission granted",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (state == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(RecordVideoActivity.this, permissions[i] + " permission denied",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                }
+                takeVideo();
                 break;
             }
         }
